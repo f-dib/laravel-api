b2170ae8858a611d042ac7956d3a100b2a6db7e4
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-30 17:16:16 +0200

    fix paginate on projectcontroller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index() {
 
-        $projects = Project::all();
+        $projects = Project::with(['type', 'technologies'])->paginate(1);
 
         
         return response()->json([
