201ae2d1ae116ead4d8b316834ae1e40ee6682c3
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-05-03 13:25:26 +0200

    added validator for email + response json completed

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,15 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
-    public function store() {
+    public function store(Request $request) {
 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'address' => 'required|email',
+            'message' => 'required'
+        ], [
+            'name.required' => "Devi inserire il tuo nome",
+            'address.required' => "Devi inserire la tua email",
+            'address.email' => "Devi inserire una mail corretta",
+            'message.required' => "Devi inserire un messaggio",
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+            
+        }
+
+        $newLead = new Lead();
+        $newLead->fill($request->all());
+        $newLead->save();
+
+        Mail::to('mailgruppo@gmail.com')->send(new NewContact($newLead));
 
         return response()->json([
             'success' => true,
+            'message' => 'Richiesta di contatto inviata correttamente',
+            'request' => $request->all(),
         ]);
 
     }
